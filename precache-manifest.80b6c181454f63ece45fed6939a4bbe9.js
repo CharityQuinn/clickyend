4633f00c0789e5e83336929398b413e0186b2a74
CharityQuinn <charityaquinn@gmail.com>
2019-03-06 09:20:39 -0500

    Updates

---
+++ precache-manifest.80b6c181454f63ece45fed6939a4bbe9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cf647c6305e1f599e4c",
+    "url": "/clickyend/static/js/runtime~main.1cf647c6.js"
+  },
+  {
+    "revision": "710ea99ff4ab35313aa9",
+    "url": "/clickyend/static/js/main.710ea99f.chunk.js"
+  },
+  {
+    "revision": "c327e0905abffeb7ddf0",
+    "url": "/clickyend/static/js/2.c327e090.chunk.js"
+  },
+  {
+    "revision": "7a159b38b36e6c040694c0afe60b18db",
+    "url": "/clickyend/index.html"
+  }
+];
